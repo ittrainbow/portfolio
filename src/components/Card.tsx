a1cef4dc1824d0385c6e8dd9c705cbb011d2b7cb
Andrew Gordienko <50226581+ittrainbow@users.noreply.github.com>
2023-11-09 20:14:41 +0300

    animates

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -27,15 +27,18 @@ export const Card = ({ title, description, stack, git, imgUrl, url, icons, apk }
   const storage = getStorage(app)
 
   useEffect(() => {
-    !aboutInViewport && !homeInViewport && !projectsInViewport && isInViewport && setProjectsInViewport(isInViewport) // eslint-disable-next-line
+    if (!aboutInViewport && !homeInViewport && !projectsInViewport && isInViewport) {
+      setProjectsInViewport(isInViewport)
+    } // eslint-disable-next-line
   }, [isInViewport])
 
   const clickHandler = () => getDownloadURL(ref(storage, apk)).then((url) => (window.location.href = url))
+  const flex = ``
 
-  const onHover = `hover:top-1/2 hover:opacity-95`
+  const onHover = `hover:opacity-95 w-full h-full`
   const cardContentClass = `${onHover} ${commonTransitionStyle} 
-    px-2 py-5 -translate-x-1/2 -translate-y-1/2 absolute top-2/3 left-1/2 flex flex-col opacity-0 min-w-[320px]`
-  const cardBgClass = `${onHover} ${commonTransitionStyle} relative max-w-md overflow-hidden rounded-xl card-bg`
+    px-2 py-5 absolute top-1 left-1 flex flex-col opacity-0 min-w-[320px]`
+  const cardBgClass = `${commonTransitionStyle} ${flex} relative max-w-md overflow-hidden rounded-xl card-bg`
   const subTextClass = 'text-sm text-center opacity-70 mt-1'
 
   return (
@@ -44,27 +47,27 @@ export const Card = ({ title, description, stack, git, imgUrl, url, icons, apk }
         <div className={cardBgClass}>
           <img src={imgUrl} alt="" />
           <div className={cardContentClass}>
-            <div className="flex flex-col text-center grow ">
-              <h4 className="px-5 mb-2 text-2xl font-bold grow">{title}</h4>
-              <div className="px-5 text-md grow">{description}</div>
-              <div className="px-5 text-lg grow">{stack}</div>
-            </div>
-            <div className="bottom-0 flex flex-row justify-between px-2 mt-3">
-              <div>
-                <div className={stackStyles}>
-                  {icons.map((el: JSX.Element, index: number) => {
-                    return <div key={index}>{el}</div>
-                  })}
+            <div className="flex flex-col justify-center text-center grow">
+              <h4 className="px-5 text-2xl font-bold">{title}</h4>
+              <div className="px-5 text-md ">{description}</div>
+              <div className="px-5 text-lg">{stack}</div>
+              <div className="bottom-0 flex flex-row px-2 mt-3 justify-evenly">
+                <div>
+                  <div className={stackStyles}>
+                    {icons.map((el: JSX.Element, index: number) => {
+                      return <div key={index}>{el}</div>
+                    })}
+                  </div>
+                  <div className={subTextClass}>Tech</div>
                 </div>
-                <div className={subTextClass}>Tech</div>
-              </div>
-              <div>
-                <div className={stackStyles + ' text-right'}>
-                  <div onClick={clickHandler}>{apk && icon.downloadLink}</div>
-                  {url && url.length > 0 && icon.externalLink(url)}
-                  {git && git.length > 0 && icon.githubLink(git)}
+                <div>
+                  <div className={stackStyles + ' text-right'}>
+                    <div onClick={clickHandler}>{apk && icon.downloadLink}</div>
+                    {url && url.length > 0 && icon.externalLink(url)}
+                    {git && git.length > 0 && icon.githubLink(git)}
+                  </div>
+                  <div className={subTextClass}>Links</div>
                 </div>
-                <div className={subTextClass}>Links</div>
               </div>
             </div>
           </div>
